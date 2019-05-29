2812c28015b7c5b2c1129447eba8b45b36cd9629
Sam Leavens <rbwsam@gmail.com>
2019-05-29 15:40:34 -0700

    Adding swappiness.sh script

---
+++ scripts/swappiness.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Derived from https://help.ubuntu.com/community/SwapFaq
+
+if [ "$EUID" -ne 0 ]
+  then echo "Must be run as root (or with sudo)"
+  exit
+fi
+
+target=10
+current=$(cat /proc/sys/vm/swappiness)
+
+if (( current <= target )); then
+    echo "swappiness is already set to $current, you're all good bro"
+    exit
+fi
+
+echo "swappiness is high at $current, setting to $target right now"
+sysctl -q vm.swappiness=$target
+
+echo "saving the setting for future boots"
+printf "\nvm.swappiness=$target\n" >> /etc/sysctl.conf
+
+echo "you're all set"
