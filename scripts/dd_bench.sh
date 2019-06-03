3e48e3814bf2e1fea5c0873d2d9bd5d35aa9acca
Sam Leavens <rbwsam@gmail.com>
2019-06-03 16:22:51 -0700

    dd_bench.sh - ask for sudo

--- scripts/dd_bench.sh
+++ scripts/dd_bench.sh
@@ -3,16 +3,11 @@
 COUNT=2048
 PREFIX="> "
 
-if [ "$EUID" -ne 0 ]
-  then echo "Must be run as root (or with sudo)"
-  exit
-fi
-
 echo "$PREFIX Writing..."
 dd if=/dev/zero of=tempfile bs=1M count=$COUNT conv=fdatasync,notrunc
 
 echo "$PREFIX Dropping caches..."
-echo 3 > /proc/sys/vm/drop_caches
+sudo bash -c "echo 3 > /proc/sys/vm/drop_caches"
 
 echo "$PREFIX Reading..."
 dd if=tempfile of=/dev/null bs=1M count=$COUNT
