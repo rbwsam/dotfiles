5fbd0907cdf534258ff58735171dc585122e8dc0
Sam Leavens <3139851+rbwsam@users.noreply.github.com>
2022-02-15 21:26:54 -0800

    Adding m make alias

--- config.sh
+++ config.sh
@@ -3,6 +3,8 @@ alias up="sudo apt update && sudo apt upgrade"
 alias pbcopy='xsel --clipboard --input'
 alias pbpaste='xsel --clipboard --output'
 
+alias m='make'
+
 export VISUAL="subl"
 export EDITOR="vim"
 
