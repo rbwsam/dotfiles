5aba736a478394795d34759bd8f9ba5e9f8ccacf
Sam Leavens <3139851+rbwsam@users.noreply.github.com>
2024-01-18 15:46:25 -0800

    Adding headless config

--- .bashrc
+++ .bashrc
@@ -10,25 +10,15 @@ if [[ ! -f "$SSH_AUTH_SOCK" ]]; then
     source "$XDG_RUNTIME_DIR/ssh-agent.env" >/dev/null
 fi
 
-# Start X
-if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then
-  exec startx
-fi
-
 complete -cf sudo
 
 alias ip='ip -color=auto'
-alias lock='loginctl lock-session'
-alias sus='sudo systemctl suspend'
-alias pbcopy='xsel --clipboard --input'
-alias pbpaste='xsel --clipboard --output'
 alias compose='docker-compose'
 alias bat='bat -p'
 
 alias up="yay -Syu"
 alias wip="git c -am WIP && git push"
 
-export VISUAL="subl"
 export EDITOR="vim"
 export GOPATH="$HOME/go"
 export PS1="[\[\e[33m\]\u\[\e[m\]@\[\e[33m\]\h\[\e[m\] \[\e[36m\]\W\[\e[m\]]\\$ "
--- scripts/arch.sh
+++ scripts/arch.sh
@@ -7,7 +7,7 @@ set -e
 mkdir -p ~/.ssh ~/bin ~/tmp ~/go ~/Downloads
 
 # Install packages
-sudo pacman -Sy --needed i3 dmenu xss-lock i3lock dex xorg-xset xorg-xrdb xorg-xrandr htop xsel iotop sysstat vim curl git git-delta bat jq go ttf-inconsolata transmission-gtk vlc chromium gimp fwupd udisks2 acpi bash-completion alacritty brightnessctl pulsemixer man gnome-screenshot fuse2 libappindicator-gtk3 noto-fonts noto-fonts-emoji unzip openssh
+sudo pacman -Sy --needed htop iotop sysstat vim curl git git-delta bat jq go acpi bash-completion man unzip openssh
 
 # Configure bash
 echo -e "\nsource ~/code/rbwsam/dotfiles/.bashrc" >> ~/.bashrc
@@ -15,20 +15,7 @@ echo -e "\nsource ~/code/rbwsam/dotfiles/.bashrc" >> ~/.bashrc
 # Configure git
 ln -s ~/code/rbwsam/dotfiles/.gitignoreglobal ~/.gitignoreglobal
 cp ~/code/rbwsam/dotfiles/.gitconfig ~/
-
-# Configure desktop
-gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-
 ln -s ~/code/rbwsam/dotfiles/.ssh/config ~/.ssh/config
-ln -s ~/code/rbwsam/dotfiles/.xinitrc ~/.xinitrc
-ln -s ~/code/rbwsam/dotfiles/.config/i3 ~/.config/i3
-ln -s ~/code/rbwsam/dotfiles/.config/i3status ~/.config/i3status
-ln -s ~/code/rbwsam/dotfiles/.config/alacritty ~/.config/alacritty
-ln -s ~/code/rbwsam/dotfiles/.config/picom ~/.config/picom
-ln -s ~/code/rbwsam/dotfiles/.config/gtk-3.0 ~/.config/gtk-3.0
-
-# Configure X11
-sudo cp ~/code/rbwsam/dotfiles/xorg.conf.d/* /etc/X11/xorg.conf.d/
 
 # Install yay
 mkdir ~/tmp/yay
